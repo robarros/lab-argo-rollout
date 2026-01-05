49f47ecfef5486fc5b295136629d8b1d3efa5f26
robarros <robarros@gmail.com>
2026-01-05 14:53:43 -0300

    purple

--- k6-teste.js
+++ k6-teste.js
@@ -6,15 +6,19 @@ export const options = {
       executor: 'constant-arrival-rate',
       rate: 50,              // 10 requisições
       timeUnit: '1s',        // por segundo
-      duration: '10m',        // duração do teste
-      preAllocatedVUs: 10,   // usuários virtuais alocados inicialmente
+      duration: '60m',        // duração do teste
+      preAllocatedVUs: 5,   // usuários virtuais alocados inicialmente
       maxVUs: 20,            // limite máximo de usuários
     },
   },
 };
 
+// export default function () {
+//   http.get('http://k8s-default-nginx-7a9d1cd726-3d9f7dad04599f8c.elb.us-east-1.amazonaws.com');
+// }
+
 export default function () {
-  http.get('http://k8s-default-nginx-7a9d1cd726-3d9f7dad04599f8c.elb.us-east-1.amazonaws.com');
+  http.get('https://api.virtualti.net/vendas');
 }
 
 //k6 run k6-teste.js
